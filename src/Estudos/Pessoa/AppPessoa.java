bb3232d22bfcf97f1ca53e0305c96bfe9c63df0e
LuccasAguiarLCC <luccasaguiarlcc@gmail.com>
2023-12-24 17:12:26 -0300

    feat: método Restaurante completo

--- src/Estudos/Pessoa/AppPessoa.java
+++ src/Estudos/Pessoa/AppPessoa.java
@@ -3,7 +3,9 @@ package Estudos.Pessoa;
 public class AppPessoa {
 
 	public static void main(String[] args) {
-		
+
+		Humano pessoa = new Humano();
+		pessoa.Restaurante();
 
 	}
 
--- src/Estudos/Pessoa/Humano.java
+++ src/Estudos/Pessoa/Humano.java
@@ -9,27 +9,36 @@ public class Humano {
 	
 	Scanner scan = new Scanner(System.in);
 	
-	public void restaurante() {
-		System.out.println("\nQuanto você pretende gastar no restaurante?"
-				+ "1) R$15,00 - Prato Feito (Arroz, feijão, batatas e bife)"
-				+ "2) R$25,00 - Self service"
-				+ "3) R$40,00 - Rodizio de carne");
-		String opcao = scan.next();
+	public void Restaurante() {
+		String opcao;
 		
-		switch(opcao) {
+		System.out.println("\nQuanto você pretende gastar no restaurante?"
+				+ "\n1) R$15,00 - Prato Feito (Arroz, feijão, batatas e bife)"
+				+ "\n2) R$25,00 - Self service"
+				+ "\n3) R$40,00 - Rodizio de carne");
+		opcao = scan.next();
+		if(opcao.equals("1") || opcao.equals("2") || opcao.equals("3")) {
+			switch(opcao) {
 			case "1":
 			carteira -= 15;
 			System.out.println("Você comeu um 'Prato Feito' e gastou R$15."
 					+ "\nTotal na carteira: R$" + carteira);
+			break;
 			case "2":
-				carteira -= 15;
+				carteira -= 25;
 				System.out.println("Você comeu 'Self Service' e gastou R$25."
 						+ "\nTotal na carteira: R$" + carteira);
+				break;
 			case "3":
-				carteira -= 15;
+				carteira -= 40;
 				System.out.println("Você comeu um 'Rodizio de Carne' e gastou R$40."
 						+ "\nTotal na carteira: R$" + carteira);
+				break;
+			}
+		}else {
+			System.out.println("Opção inválida!");
+			Restaurante();
 		}
-		
+		scan.close();
 	}
 }
