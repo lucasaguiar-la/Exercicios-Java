e0cd25592e13ae65bd82522600cd946d617b1c10
LuccasAguiarLCC <luccasaguiarlcc@gmail.com>
2023-12-24 14:55:32 -0300

    feat: TextoClima adicionado

--- src/Estudos/Clima/AppClima.java
+++ src/Estudos/Clima/AppClima.java
@@ -1,9 +1,21 @@
 package Estudos.Clima;
+import java.util.Scanner;
 
 public class AppClima {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		double c, k, f, re, ra;
+		String resposta, opcao;
+		
+		 c = 27;
+		 k = c + 273.15;
+		 f = (c * 1.8) + 32;
+		 re = c * 0.8;
+		 ra = (c * 1.8) + 32 + 459.67;
+		 
+		 new TextoClima();
 
 	}
 
--- src/Estudos/Clima/TextoClima.java
+++ src/Estudos/Clima/TextoClima.java
@@ -1,5 +1,23 @@
 package Estudos.Clima;
+import java.util.Scanner;
 
 public class TextoClima {
-
+	public String Texto() {
+		Scanner scan = new Scanner(System.in);
+		double c = 27;
+		String resposta;
+		
+		System.out.println("=================================================\n"
+				+ "-=== Bem-vindo ao programa de clima e tempo! ===-"
+				+ "\n=================================================\n" + 
+		"\nA temperatura está em " + c + "°C");
+		System.out.println("Você gostaria de fazer conversão para:" +
+		"\n1) Kelvin"
+		+ "\n2) Fahrenheit"
+		+ "\n3) Réaumur "
+		+ "\n4) Rankine");
+		resposta = scan.next();
+		
+		return resposta;
+	}
 }
